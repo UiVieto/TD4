35fd5a462dea9a3b9ecbde927151a7791c5d1a84
Huy Viet Nguyen <huy-viet.nguyen@polymtl.ca>
2022-03-11 09:13:11 -0500

    Introduction des nouvelles classes
    La struct film devient une classe.

--- structures_solutionnaire_td3_5.hpp
+++ structures_solutionnaire_td3_5.hpp
@@ -11,7 +11,7 @@
 using gsl::span;
 using namespace std;
 
-struct Film; struct Acteur; // Permet d'utiliser les types alors qu'ils seront défini après.
+class Film; struct Acteur; // Permet d'utiliser les types alors qu'ils seront défini après.
 
 class ListeFilms {
 public:
@@ -73,11 +73,25 @@ private:
 };
 using ListeActeurs = Liste<Acteur>;
 
-struct Film
+class Item {
+public:
+	string titre = "Item"s;
+	int anneSortie = 0;
+};
+
+class Film : public Item
 {
+public:
 	string titre, realisateur; // Titre et nom du réalisateur (on suppose qu'il n'y a qu'un réalisateur).
-	int anneeSortie=0, recette=0; // Année de sortie et recette globale du film en millions de dollars
+	int anneeSortie = 0, recette = 0; // Année de sortie et recette globale du film en millions de dollars
 	ListeActeurs acteurs;
+};
+
+class Livre : public Item
+{
+public:
+	string titre, auteur;
+	int ventes, nPages;
 };
 
 struct Acteur
